d2b8b6f7c3ab05bc97986a2f38f1ddbb6dfcc93b
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-16 20:07:24 -0400

    Working on the drb runner

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'rspec'
+gem 'parallel_tests'
+gem 'byebug'
--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -1,8 +1,13 @@
+require 'byebug'
 require 'rspec'
+
+# be parallel_rspec -n 4 spec
 
 20.times do |i|
   describe "Foo #{i}" do
+  #byebug
     it 'works' do
+    #byebug
       sleep 0.5
     end
   end
