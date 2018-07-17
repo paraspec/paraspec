69419f8121fcbccc3f5f48670f3d1cf46a6d5b35
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-16 22:42:11 -0400

    Formatting

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -6,13 +6,13 @@ require 'rspec'
 20.times do |i|
   describe "Foo #{i}" do
     describe 'sub 1' do
-      it 'works' do
+      it "works (#{i}-1)" do
         sleep 0.5
       end
     end
 
     describe 'sub 2' do
-      it 'works' do
+      it "works (#{i}-2)" do
         sleep 0.5
       end
     end
