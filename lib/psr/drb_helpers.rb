2c39cc750a4a72f05830f574c82c82b479df4988
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-19 21:09:12 -0400

    Get timeout handling working completely (hopefully)

--- lib/psr/drb_helpers.rb
+++ lib/psr/drb_helpers.rb
@@ -2,6 +2,22 @@ require 'timeout'
 
 module Psr
   module DrbHelpers
+    class TimeoutWrapper < BasicObject
+      def initialize(target, timeout)
+        @target, @timeout = target, timeout
+      end
+
+      def method_missing(m, *args)
+        ::Timeout.timeout(@timeout) do
+          @target.send(m, *args)
+        end
+      end
+
+      def respond_to?(m, *args)
+        super(m, *args) || @target.respond_to?(m, *args)
+      end
+    end
+
     # Connects to a DRb service and waits for the connection to start working.
     #
     # Interestingly, even when the remote end is up and running
@@ -17,25 +33,21 @@ module Psr
     private def drb_connect(uri)
       start_time = Time.now
       Psr.logger.debug("#{ident} Connecting to DRb")
-      remote = DRbObject.new_with_uri(uri)
+      remote = TimeoutWrapper.new(DRbObject.new_with_uri(uri), 2)
       Psr.logger.debug("#{ident} Waiting for DRb")
-      wait_for_drb do
-        begin
-          Timeout.timeout(1) do
-            # Assumes remote has a ping method
-            remote.ping
-          end
-        rescue DRb::DRbConnError, TypeError
-          raise if Time.now - start_time > 5
-          sleep 0.5
-          Psr.logger.debug("#{ident} Retrying DRb ping")
-          retry
-        rescue Timeout::Error
-          raise if Time.now - start_time > 5
-          Psr.logger.debug("#{ident} Reconnecting to DRb")
-          remote = DRbObject.new_with_uri(uri)
-          retry
-        end
+      begin
+        # Assumes remote has a ping method
+        remote.ping
+      rescue DRb::DRbConnError, TypeError
+        raise if Time.now - start_time > 5
+        sleep 0.5
+        Psr.logger.debug("#{ident} Retrying DRb ping")
+        retry
+      rescue Timeout::Error
+        raise if Time.now - start_time > 5
+        Psr.logger.debug("#{ident} Reconnecting to DRb")
+        remote = TimeoutWrapper.new(DRbObject.new_with_uri(uri), 2)
+        retry
       end
       remote
     end
