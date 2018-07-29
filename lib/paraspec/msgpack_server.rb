083e89949a3ab6192cf49cb101ad7cc1bf3b3067
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-29 17:41:38 -0400

    Finally found where the clients were getting reused, same issue as js loop scopes

--- lib/paraspec/msgpack_server.rb
+++ lib/paraspec/msgpack_server.rb
@@ -12,32 +12,32 @@ module Paraspec
     def run
       @socket = ::TCPServer.new('127.0.0.1', MASTER_APP_PORT)
       while s = @socket.accept
-        Thread.new do
-          u = unpacker(s)
-          u.each do |obj|
-            action = obj['action'].gsub('-', '_')
-            payload = obj['payload']
-            if payload
-              payload = IpcHash.new.merge(payload)
-              args = [payload]
-            else
-              args = []
-            end
+        run_processing_thread(s)
+      end
+    end
 
-            Paraspec.logger.debug_ipc("SrvReq:#{obj['id']} #{obj}")
-            result = @master.send(action, *args)
-
-            pk = packer(s)
-            resp = {result: result}
-            Paraspec.logger.debug_ipc("SrvRes:#{obj['id']} #{resp}")
-            pk.write(resp)
-            pk.flush
-            s.flush
-
-            if @master.stop?
-              exit 0
-            end
+    def run_processing_thread(s)
+      Thread.new do
+        u = unpacker(s)
+        u.each do |obj|
+          action = obj['action'].gsub('-', '_')
+          payload = obj['payload']
+          if payload
+            payload = IpcHash.new.merge(payload)
+            args = [payload]
+          else
+            args = []
           end
+
+          Paraspec.logger.debug_ipc("SrvReq:#{obj['id']} #{obj}")
+          result = @master.send(action, *args)
+
+          pk = packer(s)
+          resp = {result: result}
+          Paraspec.logger.debug_ipc("SrvRes:#{obj['id']} #{resp}")
+          pk.write(resp)
+          pk.flush
+          s.flush
         end
       end
     end
