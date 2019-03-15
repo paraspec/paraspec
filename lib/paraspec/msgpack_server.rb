249ac2b15451a1f95e88522ef1a32bc7916ca692
Oleg Pudeyev <oleg@bsdpower.com>
2019-03-15 15:37:26 -0400

    msgpack apparently provides more concurrency than we can handle

--- lib/paraspec/msgpack_server.rb
+++ lib/paraspec/msgpack_server.rb
@@ -8,6 +8,7 @@ module Paraspec
 
     def initialize(master)
       @master = master
+      @lock = Mutex.new
     end
 
     def run
@@ -29,38 +30,46 @@ module Paraspec
       Thread.new do
         u = unpacker(s)
         u.each do |obj|
-          result = nil
-          time = Benchmark.realtime do
-            action = obj['action'].gsub('-', '_')
-            payload = obj['payload']
-            if payload
-              payload = IpcHash.new.merge(payload)
-              args = [payload]
-            else
-              args = []
-            end
-
-            Paraspec.logger.debug_ipc("SrvReq:#{obj['id']} #{obj}")
-            result = @master.send(action, *args)
-
-            pk = packer(s)
-            resp = {result: result}
-            Paraspec.logger.debug_ipc("SrvRes:#{obj['id']} #{resp}")
-            if payload && payload['_noret']
-              # Requester does not want a response.
-              # We return a hash here because deserializer expects a hash.
-              # Always returning `resp` is problematic because it can
-              # be non-serializable.
-              pk.write({})
-            else
-              pk.write(resp)
-            end
-            pk.flush
-            s.flush
+          # Apparently this block may be run concurrently, which we are not
+          # equipped for.
+          @lock.synchronize do
+            process_request(s, obj)
           end
-          Paraspec.logger.debug_perf("SrvReq:#{obj['id']} #{obj['action']}: #{result} #{'%.3f msec' % (time*1000)}")
         end
       end
+    end
+
+    def process_request(s, obj)
+      result = nil
+      time = Benchmark.realtime do
+        action = obj['action'].gsub('-', '_')
+        payload = obj['payload']
+        if payload
+          payload = IpcHash.new.merge(payload)
+          args = [payload]
+        else
+          args = []
+        end
+
+        Paraspec.logger.debug_ipc("SrvReq:#{obj['id']} #{obj}")
+        result = @master.send(action, *args)
+
+        pk = packer(s)
+        resp = {result: result}
+        Paraspec.logger.debug_ipc("SrvRes:#{obj['id']} #{resp}")
+        if payload && payload['_noret']
+          # Requester does not want a response.
+          # We return a hash here because deserializer expects a hash.
+          # Always returning `resp` is problematic because it can
+          # be non-serializable.
+          pk.write({})
+        else
+          pk.write(resp)
+        end
+        pk.flush
+        s.flush
+      end
+      Paraspec.logger.debug_perf("SrvReq:#{obj['id']} #{obj['action']}: #{result} #{'%.3f msec' % (time*1000)}")
     end
   end
 end
